9b8ca74634d9841fcf8222f4dc5285870f13a4b7
v1k45 <v1k45@users.noreply.github.com>
2015-12-30 22:04:03 +0530

    Packages were missing the package data, fixed it

--- setup.py
+++ setup.py
@@ -1,32 +1,4 @@
 # -*- encoding: utf-8 -*-
-"""
-Python setup file for the django-notify-x.
-
-In order to register your app at pypi.python.org, create an account at
-pypi.python.org and login, then register your new app like so:
-
-    python setup.py register
-
-If your name is still free, you can now make your first release but first you
-should check if you are uploading the correct files:
-
-    python setup.py sdist
-
-Inspect the output thoroughly. There shouldn't be any temp files and if your
-app includes staticfiles or templates, make sure that they appear in the list.
-If something is wrong, you need to edit MANIFEST.in and run the command again.
-
-If all looks good, you can make your first release:
-
-    python setup.py sdist upload
-
-For new releases, you need to bump the version number in
-notify/__init__.py and re-run the above command.
-
-For more information on creating source distributions, see
-http://docs.python.org/2/distutils/sourcedist.html
-
-"""
 import os
 from setuptools import setup, find_packages
 import notify as app
@@ -53,6 +25,11 @@ setup(
     author_email='v1k45x@gmail.com',
     url="https://github.com/v1k45/django-notify-x",
     packages=find_packages(),
+    package_data={'notify': ['static/notify/*js',
+                             'templates/*.html',
+                             'templates/notifications/*.html',
+                             'templates/notifications/includes/*.html',
+                             'templates/notifications/includes/*.js']},
     include_package_data=True,
     install_requires=install_requires,
 
