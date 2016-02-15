9cadeac7939c641ead0ade6d002b8d88e54cdde0
Fernando Macedo <fgmacedo@gmail.com>
2016-02-15 11:26:25 -0200

    allow custom render targets

--- notify/utils.py
+++ notify/utils.py
@@ -1,14 +1,14 @@
 from django.template.loader import render_to_string
 
 
-def render_notification(notification, render_target='page', **extra):
+def render_notification(notification, render_target='', **extra):
     template_name = notification.nf_type
     template_dir = 'notifications/includes/'
 
     nf_ctx = {'notification': notification}
     nf_ctx.update(extra)
 
-    suffix = '_box' if render_target == 'box' else ''
+    suffix = '_{}'.format(render_target) if render_target and render_target != 'page' else ''
     templates = [
         "{0}{1}{2}.html".format(template_dir, template_name, suffix),
         "{0}{1}.html".format(template_dir, template_name),
@@ -16,6 +16,7 @@ def render_notification(notification, render_target='page', **extra):
     ]
 
     return render_to_string(templates, nf_ctx)
+
 
 def prefetch_relations(weak_queryset):
     """
