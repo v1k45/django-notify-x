cbbd8ebb58dd53dd85ef22d61e5bd9e56e70179f
Eden <eden.juscelino@gmail.com>
2020-04-21 17:40:41 +0100

    replace url with path

--- notify/urls.py
+++ notify/urls.py
@@ -1,7 +1,7 @@
 try:
-    from django.conf.urls import url
+    from django.conf.urls import path
 except ImportError:
-    from django.urls import url
+    from django.urls import path
 
 from notify import views as nf
 
@@ -9,11 +9,13 @@ app_name = 'notifications'
 
 
 urlpatterns = [
-    url(r'^all/$', nf.notifications, name="all"),
-    url(r'^api/update/$', nf.notification_update, name="update"),
-    url(r'^mark/$', nf.mark, name='mark'),
-    url(r'^mark-all/$', nf.mark_all, name='mark_all'),
-    url(r'^delete/$', nf.delete, name='delete'),
-    url(r'^rdr/(?P<notification_id>[\d]+)/$', nf.read_and_redirect,
+    path('all/', nf.notifications, name="all"),
+    path('api/update/', nf.notification_update, name="update"),
+    path('mark/', nf.mark, name='mark'),
+    path('mark-all/', nf.mark_all, name='mark_all'),
+    path('delete/<int:notification_id>/', nf.delete, name='delete'),
+    path('delete/', nf.delete, name='delete'),
+    path('rdr/<int:notification_id>/', nf.read_and_redirect,
         name='read_and_redirect'),
 ]
+
